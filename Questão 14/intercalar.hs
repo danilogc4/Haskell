0688276e8b801cd67a02b0b3cd8738c44b928d4a
Italo-Donato <italodonato4@gmail.com>
2021-03-28 15:47:43 -0300

    intercalar

--- Questão 14/intercalar.hs
+++ Questão 14/intercalar.hs
@@ -1,11 +1,7 @@
-linearizar :: [[Integer]] -> [Integer]
-linearizar [] = []
-linearizar (a:x) = a ++ linearizar x
+quickSort :: [Integer] -> [Integer]
+quickSort [] = []
+quickSort (a:x) = quickSort[ s | s <- x, s <= a]++[a]++quickSort[s | s <- x, s > a]
 
-qsort :: [Integer] -> [Integer]
-qsort [] = []
-qsort (a:x) = qsort[ b | b <- x, b <= a]++ [a] ++qsort[b | b <- x, b > a]
-
-intercalar :: [[Integer]] -> [Integer]
-intercalar [] = []
-intercalar (a:x) =  qsort (linearizar ([a]++x))
+intercalar :: [Integer] -> [Integer] -> [Integer]
+intercalar (a:c) (s:x)
+    | otherwise = quickSort (([a]++c)++([s]++x))
